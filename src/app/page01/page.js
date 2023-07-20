c32b75ed8e5217d5cb61abd56d3c80d4e80bd1a7
David Marom <davidmarom.pro@gmail.com>
2023-07-20 22:01:20 +0300

    added api

--- src/app/page01/page.js
+++ src/app/page01/page.js
@@ -1,10 +1,45 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useEffect, useState } from 'react';
+import { Row } from '@/components';
+import http from '../../services/http';
+import { PageContainer } from '@/components';
 
-export default function Page01() {
+const Page01 = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => { getData() }, []);
+
+  const getData = async () => {
+    setLoading(true);
+
+    const res = await http.get("/api01",);
+    console.log(res);
+    setData(res.data);
+    setLoading(false);
+  }
+
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Page 01</h1>
-    </main>
-  )
+    <Row>
+      <PageContainer>
+        <h1>Calling mongoDB</h1>
+        {loading ? <p>Calling mongoDB...</p> : <p>Done:</p>}
+        {data.map((item, index) => <div key={index}><p>{item.name} - {item.price}</p> </div>)}
+        <br />
+        <h1>Inserting data</h1>
+        <p>Name: </p><input type="text" onChange={(e) => setName(e.target.value)} />
+        <p>Price: </p><input type="number" onChange={(e) => setPrice(e.target.value)} />
+        <button onClick={async () => {
+          const res = await http.post("/api01", { name, price });
+          console.log(res);
+          getData();
+        }}>Insert</button> 
+      </PageContainer>
+    </Row>
+  );
 }
+
+export default Page01;
---
+++ src/services/db.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  // return await MongoClient.connect("mongodb+srv://maromdavid7:IwdCUmWBfZqwraXg@cluster0.d74ktnk.mongodb.net");
+  return await MongoClient.connect(process.env.DB_CONNECTION);
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db('bold');
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client, collection) {
+  const db = client.db('bold');
+  const documents = await db.collection(collection).find().toArray();
+  return documents;
+}
+
