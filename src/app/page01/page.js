3bf524ae17aa96866f3199775bb735948ac715d4
David Marom <davidmarom.pro@gmail.com>
2023-07-20 20:09:08 +0300

    first setup

--- src/app/page01/page.js
+++ src/app/page01/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Page01() {
   return (
     <main className={styles.main}>
-
+      <h1 className={styles.title}>Page 01</h1>
     </main>
   )
 }
