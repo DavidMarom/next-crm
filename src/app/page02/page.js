4797f1ebfb0c327b0e50f6f0b33000ab072e1bed
David Marom <davidmarom.pro@gmail.com>
2023-07-21 08:39:54 +0300

    added material ui

--- src/app/page02/page.js
+++ src/app/page02/page.js
@@ -3,56 +3,53 @@ import { useEffect, useState } from 'react';
 import { Row } from '@/components';
 import http from '../../services/http';
 import { PageContainer } from '@/components';
-import { DatePicker } from 'antd';
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Page02 = () => {
-  const [data, setData] = useState([
-    {
-      name: "test",
-      price: 0,
-      key: 1
-    }
-
-  ]);
+  const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => { getData() }, []);
+  useEffect(() => { getData() }, []);
 
   const getData = async () => {
     setLoading(true);
     const res = await http.get("/api01",);
-    console.log(res.data);
-    // setData(res.data);
-    // const dataSource = orgDataSource.map((item, index) => { return { ...item, key: index } })
-
+    setRows(res.data);
     setLoading(false);
   }
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Price',
-      dataIndex: 'price',
-      key: 'price',
-    },
-    {
-      title: 'Id',
-      dataIndex: '_id',
-      key: '_id',
-    }
-  ];
 
   return (
     <Row>
       <PageContainer>
         <h1>Calling mongoDB</h1>
 
-        <DatePicker />;
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="left">Price</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">{row.name}</TableCell>
+                  <TableCell align="left">{row.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            
+          </Table>
+        </TableContainer>
 
       </PageContainer>
     </Row>
