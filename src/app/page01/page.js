1258ee245673f3bfc25d2788ba24d8bd825aa358
David Marom <davidmarom.pro@gmail.com>
2023-07-29 16:36:51 +0300

    Formik

--- src/app/page01/page.js
+++ src/app/page01/page.js
@@ -1,33 +1,63 @@
 'use client'
 import { useState } from 'react';
-import { Row } from '@/components';
 import http from '../../services/http';
 import { PageContainer } from '@/components';
-import Button from '@mui/material/Button';
+import { Formik } from 'formik';
+import { Col } from '@/components';
+
 
 const Page01 = () => {
-  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
 
   return (
     <PageContainer>
       <h1>Add an item to the list</h1>
       <br />
       <h2>Insert a new record:</h2>
-      <p>Name: </p><input type="text" onChange={(e) => setName(e.target.value)} />
-      <p>Price: </p><input type="number" onChange={(e) => setPrice(e.target.value)} />
 
-      {loading ? <p>Inserting...</p> :
-        <button onClick={async () => {
-          setLoading(true);
-          await http.post("/api01", { name, price });
-          setLoading(false)
-        }}>Insert</button>
+      {
+        loading ? <h1>Loading...</h1> :
+
+
+
+          <Formik
+            initialValues={{ name: '', price: '' }}
+            validate={values => {
+              const errors = {};
+              if (!values.name) { errors.name = 'Required' }
+              else if (!/^[a-zA-Z]+$/.test(values.email)) { errors.name = 'Invalid name' }
+              return errors;
+            }}
+            onSubmit={async ({ name, price }, { setSubmitting }) => {
+              setLoading(true);
+              await http.post("/api01", { name, price });
+              setSubmitting(false);
+              setLoading(false);
+
+            }}
+          >
+            {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+
+              <form onSubmit={handleSubmit}>
+                <Col>
+                  <div>
+                    <p>Name</p>
+                    <input type="text" name="name" onChange={handleChange} onBlur={handleBlur} value={values.name} />
+                    {errors.name && touched.name && errors.name}
+                  </div>
+
+                  <div>
+                    <p>Price</p>
+                    <input type="number" name="price" onChange={handleChange} onBlur={handleBlur} value={values.price} />
+                    {errors.price && touched.price && errors.price}
+                  </div>
+
+                  <button type="submit" disabled={isSubmitting}>Submit</button>
+                </Col>
+              </form>
+            )}
+          </Formik>
       }
-
     </PageContainer>
   );
 }
